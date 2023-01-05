a63e933566a057792b049a06278b489755c25973
strangeal <bajerijustice@gmail.com>
2023-01-05 07:50:44 -0500

    add revese method test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,22 +1,33 @@
 describe Solver do
-  it "factorial" do
-    new_solver = Solver.new
+  before :each do
+    @solver = Solver.new
+end
 
-    factor = new_solver.factorial(5)
-    expect(factor).to eql(120)
+  context "factorial test for factorial method" do
+    it "factorial" do
+
+      factor = @solver.factorial(5)
+      expect(factor).to eql(120)
+    end
+
+    it "receive error for ingetive" do
+
+      factor = @solver.factorial(-5)
+      expect{factor}.to raise_error('NoNegativeIntergerAllowed')
+    end
+
+    it 'factorial of 0 should equal 1' do
+
+      factor = @solver.factorial(0)
+      expect(factor).to eql(1)
+    end
   end
 
-  it "receive error for ingetive" do
-    new_solver = Solver.new
-
-    factor = new_solver.factorial(-5)
-    expect{factor}.to raise_error('NoNegativeIntergerAllowed')
+  context "Reverse method test" do
+    it 'reverse string' do
+      factor = @solver.reverse("Name")
+      expect(factor).to eql("emaN")
+    end
   end
 
-  it 'factorial of 0 should equal 1' do
-    new_solver = Solver.new
-
-    factor = new_solver.factorial(0)
-    expect(factor).to eql(1)
-  end
 end
